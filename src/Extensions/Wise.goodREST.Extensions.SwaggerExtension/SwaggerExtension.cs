d77f1a0ad4f5509406eead1fef3ad3a4940e5869
nieprzecietny <nieprzecietny@PRECISION-5510>
2017-02-12 03:05:04 +0100

    init

---
+++ src/Extensions/Wise.goodREST.Extensions.SwaggerExtension/SwaggerExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wise.goodREST.Middleware.Interface;
+
+namespace Wise.goodREST.Extensions.SwaggerExtension
+{
+    public class SwaggerExtension : IExtension
+    {
+        public SwaggerExtension()
+        {
+
+        }
+    }
+}
