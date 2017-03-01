f0bfa94ce494fc02bff9b53b4fd81a5ed6342acc
nieprzecietny <nieprzecietny@PRECISION-5510>
2017-03-01 19:42:06 +0100

    buid schema in service serve valid json file accordingly to schema

--- src/Extensions/Wise.goodREST.Extensions.SwaggerExtension/propertyDescription.cs
+++ src/Extensions/Wise.goodREST.Extensions.SwaggerExtension/propertyDescription.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Wise.goodREST.Extensions.SwaggerExtension
 {
     public class propertyDescription
     {
+        public propertyDescription()
+        {
+            @enum = Enumerable.Empty<string>();
+        }
         public string type { get; set; }
         public string format { get; set; }
         public string description { get; set; }
