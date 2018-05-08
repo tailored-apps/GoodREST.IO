24d1365748c96a365ade7fb9fbd46bd76219b24d
Lukasz Kowalski <kowalski@nieprzecietny.pl>
2018-05-08 09:53:14 +0200

    add encoding utf to contenttype

---
+++ src/goodREST/Serializers/JsonSerializerCharsetUtf8.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using goodREST.Interfaces;
+
+namespace goodREST.Serializers
+{
+    public class JsonSerializerCharsetUtf8 : IRequestResponseSerializer
+    {
+        public string ContentType
+        {
+            get
+            {
+                return "application/json;charset=UTF-8";
+            }
+            
+        }
+
+        public string Serialize<T>(T o)
+        {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+             {
+                 Formatting = Newtonsoft.Json.Formatting.Indented,
+                 ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                 
+             };
+            return JsonConvert.SerializeObject(o);
+        }
+
+        public T Deserialize<T>(string o)
+        {
+            return JsonConvert.DeserializeObject<T>(o);
+        }
+        public object Deserialize(Type type ,string o)
+        {
+            return JsonConvert.DeserializeObject(o,type);
+        }
+
+    }
+}
