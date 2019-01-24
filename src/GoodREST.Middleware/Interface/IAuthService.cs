846f184b8df6b052abd724e9e672fceae23a71ee
Lukasz Kowalski <kowalski@nieprzecietny.pl>
2019-01-24 21:44:47 +0100

    changed interface to be able deal with rights object

--- src/GoodREST.Middleware/Interface/IAuthService.cs
+++ src/GoodREST.Middleware/Interface/IAuthService.cs
@@ -3,8 +3,8 @@
     public interface IAuthService
     {
         string AuthUrl { get; }
-        T AuthUser<T>(string login, string pass, string salt) where T : new();
+        T AuthUser<T>(string login, string pass, string salt) where T : class, new();
         string PassGen(string password, string salt);
-        T CheckAccess<T>(string xauth) where T : new();
+        T CheckAccess<T>(string xauth) where T : class, new();
     }
 }
