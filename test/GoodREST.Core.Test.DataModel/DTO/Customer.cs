4dfd6203c1ce43e75cff4a84c2577fe7d55dc1a2
Lukasz Kowalski <lukasz.kowalski@TAILOREDAPPS.PL>
2020-01-10 03:23:43 +0100

    fix issue with required flag on types

--- test/GoodREST.Core.Test.DataModel/DTO/Customer.cs
+++ test/GoodREST.Core.Test.DataModel/DTO/Customer.cs
@@ -8,6 +8,12 @@ namespace GoodREST.Core.Test.DataModel.DTO
         public string Address { get; set; }
         public DateTime CreateDate { get; set; }
         public int Status { get; set; }
-
+        public EnumOne NotNullableEnumProp { get; set; }
+        public EnumOne? NullableEnumProp { get; set; }
     }
+    public enum EnumOne  {
+        One,
+        Two
+
+        }
 }
