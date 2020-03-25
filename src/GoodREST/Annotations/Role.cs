29386446e1193d235086b8d0349b9bb488a6c6d8
Lukasz Kowalski <lukasz.kowalski@TAILOREDAPPS.PL>
2020-03-25 22:21:09 +0100

    added annotations for authorization and role based access

---
+++ src/GoodREST/Annotations/Role.cs
@@ -0,0 +1,19 @@
+﻿using GoodREST.Enums;
+using System;
+
+namespace GoodREST.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class Role : Attribute
+    {
+        public Role(params string[] codes)
+        {
+            if (codes==null | codes.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            this.Codes = codes;
+        }
+        public string[] Codes { get; }
+    }
+}
